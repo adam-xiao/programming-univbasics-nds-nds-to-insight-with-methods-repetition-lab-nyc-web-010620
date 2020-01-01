f271012208251383cd32a195ab0f9dd263a8a3a7
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-01 02:38:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  director_all = directors_totals(source)
+  directors_totals("Stephen Spielberg")
 
 
 
