aba33f9e56432312674a50d29ea802132c7f8236
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-01 02:35:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,8 +46,9 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  director_all = list_of_directors(source)
-  gross_for_director(director_all[0])
+  director_all = directors_totals(source)
+   
+  
 
 
 #  total = 0
